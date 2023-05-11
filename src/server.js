95467f317e0d2b6e96da52d871beb19240111fd6
Carlos <carlos.silva@compasso.com.br>
2023-05-11 17:06:12 -0300

    feat: adding route to users and creating middleware

--- src/server.js
+++ src/server.js
@@ -2,8 +2,39 @@ import http from "node:http";
 
 const PORT = 3333;
 
-const server = http.createServer((req, res) => {
-  return res.end("Initial setup");
+const users = [];
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
+    users.push({
+      id: new Date().getTime().toString(),
+      name,
+      email,
+    });
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end();
 });
 
 server.listen(
